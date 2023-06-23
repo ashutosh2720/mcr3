bf710be67bd1e81899ad34e7859f897dee3a9b9d
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2023-06-23 19:11:36 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-black text-white">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline bg-black text-white text-center p-4">
+        Hello world!
+      </h1>
     </>
   );
 }
